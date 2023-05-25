0d56a2c81c17a22304122d1bca326bd741bca4ac
OdilovMM <133968972+OdilovMM@users.noreply.github.com>
2023-05-25 18:37:24 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const tagsEl = document.getElementById("tags");
+
+const textArea = document.getElementById("textarea");
+
+textArea.focus();
+
+textArea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = " ";
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  tagsEl.innerHTML = "";
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerHTML = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = picRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      uNhighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = picRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function picRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function uNhighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
